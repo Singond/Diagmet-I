bbd12871d60795e4b6af65b05ed8c0c1f514cd56
Jan Singon Slany <singond@seznam.cz>
2023-02-03 23:05:21 +0100

    02: Describe ionization energy measurement

---
+++ 02_corpuscular/ionization_export.m
@@ -0,0 +1,47 @@
+pkg load report;
+
+argon;
+
+gp = gnuplotter();
+gp.load("plotsettings.gp");
+gp.exec("\n\
+	set decimalsign '.' \n\
+	set lmargin 6 \n\
+	set rmargin 2 \n\
+	set tmargin 2 \n\
+	set xrange [15:20] \n\
+	set xlabel '$\\en\\,[\\si{\\electronvolt}]$' \n\
+	set key top left \n\
+	set title '\\ce{Ar+}' \n\
+");
+in = max(in, 1);
+gp.plot(ar1.E,    ar1.in,    "w l ls 1      t 'data'");
+gp.plot(ar1.fitE, ar1.fitin, "w l ls 2 dt 2 t 'fit'");
+gp.exec(sprintf("\n\
+	set arrow from %f,%f rto character 0,2 ls 1 as 1 backhead \n\
+	set label center at %f,%f offset character 0,3 '\\SI{%.2f}{\\electronvolt}' \n\
+	",
+	ar1.Ei, 0, ar1.Ei, 0, ar1.Ei));
+gp.export("plots/ionization-1.tex", "epslatex", "size 8cm,7cm");
+
+gp = gnuplotter();
+gp.load("plotsettings.gp");
+gp.exec("\n\
+	set decimalsign '.' \n\
+	set lmargin 6 \n\
+	set rmargin 2 \n\
+	set tmargin 2 \n\
+	set xrange [40:60] \n\
+	set xlabel '$\\en\\,[\\si{\\electronvolt}]$' \n\
+	set key top left \n\
+	set title '\\ce{Ar^2+}' \n\
+");
+in = max(in, 1);
+gp.plot(ar2.E,    ar2.in,    "w l ls 1      t 'data'");
+gp.plot(ar2.fitE, ar2.fitin, "w l ls 2 dt 2 t 'fit'");
+gp.exec(sprintf("\n\
+	set arrow from %f,%f rto character 0,2 ls 1 as 1 backhead \n\
+	set label center at %f,%f offset character 0,3 '\\SI{%.2f}{\\electronvolt}' \n\
+	",
+	ar2.Ei, 0, ar2.Ei, 0, ar2.Ei));
+gp.export("plots/ionization-2.tex", "epslatex", "size 8cm,7cm");
---
+++ 02_corpuscular/plotsettings.gp
@@ -0,0 +1,3 @@
+load "../plotsettings.gp"
+
+set style arrow 1 filled size character 1,15,60 fixed
