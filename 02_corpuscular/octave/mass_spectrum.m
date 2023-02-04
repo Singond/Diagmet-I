11a76aba008549600339ee1c0b4aa5d820a739fb
Jan Singon Slany <singond@seznam.cz>
2023-02-04 14:20:16 +0100

    02: Fix index out of bounds error in Octave function

--- 02_corpuscular/octave/mass_spectrum.m
+++ 02_corpuscular/octave/mass_spectrum.m
@@ -1,6 +1,7 @@
 function s = mass_spectrum(mz, in)
 	s.mz = (1:ceil(max(mz)))';
 	[~, idx] = histc(mz, [s.mz - 0.5; s.mz(end) + 0.5]);
+	idx = max(idx, 1);
 	s.in = accumarray(idx, in, [], @mean);
 	s.inmax = accumarray(idx, in, [], @max);
 endfunction
